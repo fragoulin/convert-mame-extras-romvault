09344d49c0017fe2b27c38f2d8d78f6469f71d26
Fabrice Morin <fabrice.morin@live.com>
2024-05-01 14:44:25 +0200

    Fix build for linux

--- build.sh
+++ build.sh
@@ -8,15 +8,15 @@ if [ -z "$1" ]
 
         echo "Building for version $1"
 
-        declare -A envs=(
-            ["win32"]="i686-pc-windows-gnu/release/convert-mame-extras-romvault.exe"
-            ["win64"]="/x86_64-pc-windows-gnu/release/convert-mame-extras-romvault.exe"
-            ["linux64"]="/release/convert-mame-extras-romvault"
+        declare -A cmds=(
+            ["win32"]="zip -j releases/convert_mame_extras_romvault-win32-$1.zip target/i686-pc-windows-gnu/release/convert-mame-extras-romvault.exe"
+            ["win64"]="zip -j releases/convert_mame_extras_romvault-win64-$1.zip target/x86_64-pc-windows-gnu/release/convert-mame-extras-romvault.exe"
+            ["linux64"]="tar -cvzf releases/convert_mame_extras_romvault-linux64-$1.tgz --directory=target/release convert-mame-extras-romvault"
         )
 
-        for env in "${!envs[@]}"; do
+        for env in "${!cmds[@]}"; do
             echo "build $env"
             cargo "build_$env"
-            zip -j releases/convert_mame_extras_romvault-$env-$1.zip target/${envs[$env]}
+            ${cmds[$env]}
         done
 fi
