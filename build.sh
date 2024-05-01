148750c8165354bb8a6018ce68d0d28a2f8a8e89
Fabrice Morin <fabrice.morin@live.com>
2024-05-01 22:55:32 +0200

    Move releases folder into target

--- build.sh
+++ build.sh
@@ -4,14 +4,15 @@ if [ -z "$1" ]
     then
         echo "Please supply version"
     else
-        rm releases/*
+        mkdir -p target/releases
+        rm target/releases/*
 
         echo "Building for version $1"
 
         declare -A cmds=(
-            ["win32"]="zip -j releases/convert_mame_extras_romvault-win32-$1.zip target/i686-pc-windows-gnu/release/convert-mame-extras-romvault.exe"
-            ["win64"]="zip -j releases/convert_mame_extras_romvault-win64-$1.zip target/x86_64-pc-windows-gnu/release/convert-mame-extras-romvault.exe"
-            ["linux64"]="tar -cvzf releases/convert_mame_extras_romvault-linux64-$1.tgz --directory=target/release convert-mame-extras-romvault"
+            ["win32"]="zip -j target/releases/convert_mame_extras_romvault-win32-$1.zip target/i686-pc-windows-gnu/release/convert-mame-extras-romvault.exe"
+            ["win64"]="zip -j target/releases/convert_mame_extras_romvault-win64-$1.zip target/x86_64-pc-windows-gnu/release/convert-mame-extras-romvault.exe"
+            ["linux64"]="tar -cvzf target/releases/convert_mame_extras_romvault-linux64-$1.tgz --directory=target/release convert-mame-extras-romvault"
         )
 
         for env in "${!cmds[@]}"; do
