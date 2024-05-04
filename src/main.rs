2ec82ab7195e2555a055ed72b27178f7a33a5d75
Fabrice Morin <fabrice.morin@live.com>
2024-05-04 20:30:19 +0200

    Use i8 for return code

--- src/main.rs
+++ src/main.rs
@@ -25,5 +25,5 @@ use convert_mame_extras_romvault::real_main;
 fn main() {
     let args = env::args().skip(1).collect::<Vec<_>>();
     let code = real_main(&args);
-    process::exit(code);
+    process::exit(code.into());
 }
